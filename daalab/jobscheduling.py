faa02afd3fff78df6e9ce94568f256baa4048bcf
Devika Shetty <124135758+shettydevika@users.noreply.github.com>
2023-07-27 06:41:29 +0000

    commit

--- daalab/jobscheduling.py
+++ daalab/jobscheduling.py
@@ -27,3 +27,28 @@ if __name__ =='__main__':
         jobs.append(Job(taskId, deadline, profit))
     T = int(input("Enter the total time: "))
     schedulejobs(jobs, T)
+    
+'''
+Output:
+Enter the number of jobs: 6
+Enter task ID for job 1: 1
+Enter deadline for job 1: 2
+Enter profit for job 1: 200
+Enter task ID for job 2: 2
+Enter deadline for job 2: 3
+Enter profit for job 2: 180
+Enter task ID for job 3: 3
+Enter deadline for job 3: 2
+Enter profit for job 3: 120
+Enter task ID for job 4: 4
+Enter deadline for job 4: 3
+Enter profit for job 4: 100
+Enter task ID for job 5: 5
+Enter deadline for job 5: 4
+Enter profit for job 5: 50
+Enter task ID for job 6: 6
+Enter deadline for job 6: 1
+Enter profit for job 6: 30
+Enter the total time: 4
+The scheduled jobs are ['3', '1', '2', '5']
+The total profit earned is 550'''
